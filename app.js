c0c5859b43c36adfd82119093c3fda127dd78cd1
shelia <woman5650@gmainl.com>
2020-01-28 19:26:12 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,144 @@
+
+// CONDITIONALS
+
+// if (true){
+//     // do stuff
+// };
+
+// let foo = 'hello';
+// if (foo == 'hello'){
+//     console.log('TRUE');
+    
+// };
+
+// let bar = 2;
+// if (bar < 2){
+//     console.log('TRUE');
+// };
+/*
+= assignment
+== is equal to
+=== strictly equal to
+< less
+> greater
+<= less or equal
+>= greater or equal
+!= not equal
+!= not strictly equal
+*/
+
+// console.log(bar < 2);
+
+// if (bar > 3) {
+//     console.log('maybe true');
+    
+// } else {
+//     console.log('totally false');
+    
+// };
+
+// if (bar > 3) {
+//     console.log('yes');
+    
+// }
+//  else if(bar > 1){
+//     console.log('yep');
+    
+// }
+// else{
+//     console.log('nahhh'); 
+// }
+
+//switches used strict comparison
+
+// bar = 0
+// switch (bar) {
+//     case 0:
+//         console.log('bar is 0');
+        
+//         break;
+//     case 2:
+//         console.log('bar is 1');
+        
+//         break;
+//     case 3:
+//         console.log('bar is 2');
+        
+//         break;
+//     default:
+//         break;
+// };
+// // foo = 'hola';
+// switch (foo) {
+//     case 'hello':
+//     case 'hola':
+//         console.log('Foop There');
+//     default:
+//         break;
+// }
+
+// console.log('a' > 'Z'); lowercase keys are greater than uppercase
+
+//AND / OR
+
+// console.log(1<2 && 1<0);
+// console.log(1<2 && 1>0);
+
+// console.log(1<2 || 1<0);
+// Decision Tree
+// Pet Finder:
+// ---------------------
+// |_1.Dog 2.Cat
+// ---------------------
+// |
+// |_3.Snake 4.Fish
+// ---------------------
+
+// Do you want a pet with fur?
+if (true) {
+    // DOG/CAT
+    // Do you want an independant pet?
+    if (true) {
+        // CAT
+    } else {
+        // DOG
+    }
+ } else {
+     // SNAKE/FISH
+     // Do you want a pet you can hold?
+     if (true) {
+         // SNAKE
+     } else {
+         // FISH
+         {
+     {
+         // TRUTHY and FALSEY  
+         /*
+         Number 0 -> Falsey
+         false -> Falsey
+         
+         Empty String "" -> Falsey
+         undefined -> Falsey
+         null -> Falsey
+         NaN -> Falsey
+         */
+        let num = 0;
+        if (num) {
+            console.log('here');
+        }
+        let bool = true;
+        if (!bool){
+            console.log('not here');
+        if (num) {
+            console.log('here');
+            {
+
+        let array = [1,2,3]
+        let obj = {
+
+
+
+         
+
+
+
